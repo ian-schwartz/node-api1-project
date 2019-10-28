adf9c8465220c43d56a013c91cf39427f7457991
Ian Schwartz <ian.schwartz23@gmail.com>
2019-10-28 14:01:21 -0700

    POST

--- index.js
+++ index.js
@@ -29,6 +29,18 @@ server.get('/api/users/:id', (req, res) => {
 
 
 // POST to /api/users
+server.post('/api/users', (req, res) => {
+    if (!req.body.name || !req.body.bio) {
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user." });
+    } else {
+        db.insert(req.body)
+          .then(user => {
+            res.status(201).json(user);
+          })
+          .catch(() => res.status(500).json({ error: "There was an error while saving the user to the database" }));
+        }
+});
+
 
 
 const port = 8000;
